5d2db71a4957880e16f9117ae2f949a6c4822aa3
lambdazen <lambdazen@937fe5dd-4196-4564-82b1-5ae60724d619>
2012-05-28 21:30:42 +0000

    Org imports

--- trunk/Samples/src/main/java/com/lambdazen/websocket/sample/TempConversionServlet.java
+++ trunk/Samples/src/main/java/com/lambdazen/websocket/sample/TempConversionServlet.java
@@ -1,20 +1,10 @@
 package com.lambdazen.websocket.sample;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.eclipse.jetty.websocket.WebSocket;
-import org.eclipse.jetty.websocket.WebSocketFactory;
 
 import com.lambdazen.websocket.AbstractRMIWebSocketServlet;
 import com.lambdazen.websocket.IRMIWebSocket;
 import com.lambdazen.websocket.IRMIWebSocketListener;
-import com.lambdazen.websocket.IRMIWebSocketListenerFactory;
-import com.lambdazen.websocket.RMIWebSocket;
 
 // Skeleton code from http://webtide.intalio.com/2011/08/websocket-example-server-client-and-loadtest/
 public class TempConversionServlet extends AbstractRMIWebSocketServlet {
